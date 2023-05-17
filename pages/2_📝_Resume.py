6d842480fb6095811b0dcf027f9da3e0c1da98d2
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-05-17 19:23:57 -0400

    Update 2_📝_Resume.py

--- pages/2_📝_Resume.py
+++ pages/2_📝_Resume.py
@@ -8,7 +8,7 @@ st.title("📝 Resume")
 
 st.write("[Click here if it's blocked by your browser](https://drive.google.com/file/d/1e_trgOgzwYIY6eMSvjmyjgqBcCOdueZ1/view?usp=sharing)")
 
-with open("website-main/images/resume.pdf","rb") as f:
+with open("images/resume.pdf","rb") as f:
       base64_pdf = base64.b64encode(f.read()).decode('utf-8')
       pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
       st.markdown(pdf_display, unsafe_allow_html=True)
