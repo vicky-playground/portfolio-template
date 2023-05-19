a44671751dee68593ed2f63b905d49f014b6fc17
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-05-18 23:51:53 -0400

    Update 2_📝_Resume.py

--- pages/2_📝_Resume.py
+++ pages/2_📝_Resume.py
@@ -2,6 +2,12 @@ import streamlit as st
 import base64
 from constant import *
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+        
+local_css("style/style.css")
+
 st.sidebar.markdown(info['Photo'],unsafe_allow_html=True)
 
 st.title("📝 Resume")
