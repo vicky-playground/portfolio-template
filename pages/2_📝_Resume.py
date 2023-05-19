76f28ce8de6eaccf9b923dc9220e200713c1b08d
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-05-19 00:00:53 -0400

    Update 2_📝_Resume.py

--- pages/2_📝_Resume.py
+++ pages/2_📝_Resume.py
@@ -18,3 +18,10 @@ with open("images/resume.pdf","rb") as f:
       base64_pdf = base64.b64encode(f.read()).decode('utf-8')
       pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
       st.markdown(pdf_display, unsafe_allow_html=True)
+        
+# -----------------  footer  ----------------- #
+footer="""
+<div class="footer">
+<p>Developed with Streamlit by <a href={} target="_blank">Vicky Kuo @ IBM</a></p></div>
+""".format(foot['url'])
+st.markdown(footer,unsafe_allow_html=True)
