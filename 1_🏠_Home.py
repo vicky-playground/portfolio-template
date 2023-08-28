c433e6825dab6925f247a602d4661a1a63ef524c
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-08-28 03:25:40 -0400

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -264,6 +264,6 @@ with st.container():
 # -----------------  footer  ----------------- #
 footer="""
 <div class="footer">
-<p>Developed with Streamlit by <a href={} target="_blank">Vicky Kuo @ IBM</a></p></div>
+<p>Developed with Streamlit by <a href={} target="_blank">Vicky Kuo</a></p></div>
 """.format(foot['url'])
 st.markdown(footer,unsafe_allow_html=True)
