63d8a372c040df29754cd5d1963189e3d6f00804
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-09-12 14:40:39 -0400

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -76,7 +76,9 @@ if user_input:
     st.info(ask_bot(user_input))
 
 # -----------------  loading assets  ----------------- #
-st.sidebar.markdown(info['Photo'],unsafe_allow_html=True)
+rounded_image_html = f'<img src="{info["Photo"]}" style="border-radius: 50%; width: 100px; height: 100px;">'
+st.sidebar.markdown(rounded_image_html, unsafe_allow_html=True)
+
     
 def load_lottieurl(url: str):
     r = requests.get(url)
