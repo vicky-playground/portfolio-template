e6b2044bab84313adc0fcc63654d3e76c0a4a611
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-09-20 23:26:34 -0400

    Update 2_Resume.py

--- pages/2_Resume.py
+++ pages/2_Resume.py
@@ -23,5 +23,5 @@ with open("images/resume.pdf","rb") as f:
 footer="""
 <div class="footer">
 <p>Made by <a href="https://cognitiveclass.ai/" target="_blank">Cognitive Class</a></p></div>
-""".format(foot['url'])
+"""
 st.markdown(footer,unsafe_allow_html=True)
