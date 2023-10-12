1f70aea17b0b00409c3d8bfc5e70bbb4c8c849cc
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-12 02:06:08 -0400

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -181,70 +181,69 @@ with st.container():
     col1,col2,col3 = st.columns([0.475, 0.475, 0.05])
         
     with col1:
-        st.write("---")
-        st.subheader("💬 See how my coworker describe me:")
+        st.subheader("👄 Coworker Endorsements")
         components.html(
-        """
+        f"""
         <!DOCTYPE html>
         <html>
         <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <style>
-            * {box-sizing: border-box;}
-            .mySlides {display: none;}
-            img {vertical-align: middle;}
+            * {{box-sizing: border-box;}}
+            .mySlides {{display: none;}}
+            img {{vertical-align: middle;}}
 
             /* Slideshow container */
-            .slideshow-container {
+            .slideshow-container {{
             position: relative;
             margin: auto;
             width: 100%;
-            }
+            }}
 
             /* The dots/bullets/indicators */
-            .dot {
+            .dot {{
             height: 15px;
             width: 15px;
             margin: 0 2px;
-            background-color: #6F6F6F;
+            background-color: #eaeaea;
             border-radius: 50%;
             display: inline-block;
             transition: background-color 0.6s ease;
-            }
+            }}
 
-            .active {
-            background-color: #eaeaea;
-            }
+            .active {{
+            background-color: #6F6F6F;
+            }}
 
             /* Fading animation */
-            .fade {
+            .fade {{
             animation-name: fade;
             animation-duration: 1s;
-            }
+            }}
 
-            @keyframes fade {
-            from {opacity: .4} 
-            to {opacity: 1}
-            }
+            @keyframes fade {{
+            from {{opacity: .4}} 
+            to {{opacity: 1}}
+            }}
 
             /* On smaller screens, decrease text size */
-            @media only screen and (max-width: 300px) {
-            .text {font-size: 11px}
-            }
+            @media only screen and (max-width: 300px) {{
+            .text {{font-size: 11px}}
+            }}
             </style>
         </head>
         <body>
             <div class="slideshow-container">
                 <div class="mySlides fade">
-                <img src="https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png" style="width:100%">
+                <img src={endorsements["img1"]} style="width:100%">
                 </div>
 
                 <div class="mySlides fade">
-                <img src="https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png" style="width:100%">
+                <img src={endorsements["img2"]} style="width:100%">
                 </div>
 
                 <div class="mySlides fade">
-                <img src="https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png" style="width:100%">
+                <img src={endorsements["img3"]} style="width:100%">
                 </div>
 
             </div>
@@ -260,38 +259,38 @@ with st.container():
             let slideIndex = 0;
             showSlides();
 
-            function showSlides() {
+            function showSlides() {{
             let i;
             let slides = document.getElementsByClassName("mySlides");
             let dots = document.getElementsByClassName("dot");
-            for (i = 0; i < slides.length; i++) {
+            for (i = 0; i < slides.length; i++) {{
                 slides[i].style.display = "none";  
-            }
+            }}
             slideIndex++;
-            if (slideIndex > slides.length) {slideIndex = 1}    
-            for (i = 0; i < dots.length; i++) {
+            if (slideIndex > slides.length) {{slideIndex = 1}}    
+            for (i = 0; i < dots.length; i++) {{
                 dots[i].className = dots[i].className.replace("active", "");
-            }
+            }}
             slides[slideIndex-1].style.display = "block";  
             dots[slideIndex-1].className += " active";
-            }
+            }}
 
-            var interval = setInterval(showSlides, 1200); // Change image every 1.2 seconds
+            var interval = setInterval(showSlides, 2500); // Change image every 2.5 seconds
 
             function pauseSlides(event)
-            {
+            {{
                 clearInterval(interval); // Clear the interval we set earlier
-            }
+            }}
             function resumeSlides(event)
-            {
-                interval = setInterval(showSlides, 1200);
-            }
+            {{
+                interval = setInterval(showSlides, 2500);
+            }}
             // Set up event listeners for the mySlides
             var mySlides = document.getElementsByClassName("mySlides");
-            for (i = 0; i < mySlides.length; i++) {
+            for (i = 0; i < mySlides.length; i++) {{
             mySlides[i].onmouseover = pauseSlides;
             mySlides[i].onmouseout = resumeSlides;
-            }
+            }}
             </script>
 
             </body>
