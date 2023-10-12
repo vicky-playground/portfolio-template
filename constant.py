9a02f92500d49c28fcd6d3a3dddd188fc045023a
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-12 00:20:26 -0400

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,22 +1,22 @@
 info = {
-   "About":"Hey there, I'm Buddy! I'm passionate about sharing my technical and data knowledge. My expertise covers a wide range of topics, from AI and Data Science to Blockchain, Cloud, and more. You can explore the wealth of free courses and resources I've created at cognitiveclass.ai.",
+   "Pronoun": "her", 
+   "Name": "Vicky",
+   "Full_Name" = "Vicky Kuo",
+   "Intro": "A Tech Educator and AI Enthusiast at cognitiveclass.ai",
+   "About":"Hey there, I'm Vicky! I'm passionate about sharing my technical and data knowledge. My expertise covers a wide range of topics, from AI and Data Science to Blockchain, Cloud, and more. You can explore the wealth of free courses and resources I've created at cognitiveclass.ai.",
    "Tableau":"https://public.tableau.com/app/profile/vickytc/viz/SunnybrookTeam/Overview",
-   "Medium":"https://medium.com/the-power-of-ai/about",
+   "Medium":"https://medium.com/@vicky-note/about",
    "City":"Toronto, Canada",
-   "Photo":"""<a href=\"https://cognitiveclass.ai/\"><img src=\"https://sn-portals-cognitiveclass.s3.us-south.cloud-object-storage.appdomain.cloud/lmzotycs3p11f936gx1u5drchdrk" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>"""
+   "Photo":"""<a href=\"https://www.linkedin.com/in/vicky-tck/\"><img src=\"https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/profile.png" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>"""
 }
 
 embed_rss= {
     'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
-        data-rss="https://medium.com/feed/the-power-of-ai"
+        data-rss="https://medium.com/feed/vicky-note"
         data-maxcols="3" 
         data-layout="grid"
         data-poststyle="inline" 
         data-readmore="Read the rest" 
         data-buttonclass="btn btn-primary" 
         data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
-}
-
-foot = {
-    "url":"https://www.linkedin.com/in/vicky-tck/"
 }
