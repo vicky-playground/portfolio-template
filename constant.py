dee8f0eb9e4bddc7a40d33a9d9dfe2457cd4c418
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-12 02:06:47 -0400

    Update constant.py

--- constant.py
+++ constant.py
@@ -21,3 +21,9 @@ embed_rss= {
         data-buttonclass="btn btn-primary" 
         data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
 }
+
+endorsements = {
+    "img1": "https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png",
+    "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
+    "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
+}
