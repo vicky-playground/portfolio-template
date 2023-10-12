2559b22bf2a4b63b01337be60da7028e94706b3d
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-12 00:31:05 -0400

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,7 +1,7 @@
 info = {
    "Pronoun": "her", 
    "Name": "Vicky",
-   "Full_Name" = "Vicky Kuo",
+   "Full_Name": "Vicky Kuo",
    "Intro": "A Tech Educator and AI Enthusiast at cognitiveclass.ai",
    "About":"Hey there, I'm Vicky! I'm passionate about sharing my technical and data knowledge. My expertise covers a wide range of topics, from AI and Data Science to Blockchain, Cloud, and more. You can explore the wealth of free courses and resources I've created at cognitiveclass.ai.",
    "Tableau":"https://public.tableau.com/app/profile/vickytc/viz/SunnybrookTeam/Overview",
