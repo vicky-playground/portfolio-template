205993b21327407684901d9ff9fd650f05fd63e0
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-12 00:31:43 -0400

    Update 2_Resume.py

--- pages/2_Resume.py
+++ pages/2_Resume.py
@@ -18,10 +18,4 @@ with open("images/resume.pdf","rb") as f:
       base64_pdf = base64.b64encode(f.read()).decode('utf-8')
       pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
       st.markdown(pdf_display, unsafe_allow_html=True)
-        
-# -----------------  footer  ----------------- #
-footer="""
-<div class="footer">
-<p>Made by <a href="https://cognitiveclass.ai/" target="_blank">Cognitive Class</a></p></div>
-"""
-st.markdown(footer,unsafe_allow_html=True)
+  
