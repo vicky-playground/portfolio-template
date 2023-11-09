15e4dceacd5e11aa486bfb5b528586efc08e8a2d
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-11-08 22:05:58 -0500

    Update constant.py

--- constant.py
+++ constant.py
@@ -3,7 +3,7 @@ info = {
    "Name": "Vicky",
    "Full_Name": "Vicky Kuo",
    "Intro": "A Tech Educator and AI Enthusiast at cognitiveclass.ai",
-   "About":"Hey there, I'm Vicky! I'm passionate about sharing my technical and data knowledge. My expertise covers a wide range of topics, from AI and Data Science to Blockchain, Cloud, and more. You can explore the wealth of free courses and resources I've created at cognitiveclass.ai.",
+   "About":"Hey there, I'm Vicky! I'm passionate about leveraging data and technology to drive meaningful insights and solutions in business. You can explore the wealth of free courses and resources I've created at cognitiveclass.ai.",
    "Tableau":"https://public.tableau.com/app/profile/vickytc/viz/SunnybrookTeam/Overview",
    "Medium":"https://medium.com/@vicky-note/about",
    "City":"Toronto, Canada",
